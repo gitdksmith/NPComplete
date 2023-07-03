0a532b16bf1c5394dc16692081153d3765c648c4
gitdksmith <dksmith@protonmail.com>
2023-07-03 18:45:32 -0500

    #2

--- tests/features/step_definitions/home_page.js
+++ tests/features/step_definitions/home_page.js
@@ -15,11 +15,7 @@ capabilities.set('chromeOptions', {
     "headless": true,
     args: ['headless', 'disable-gpu', 'no-sandbox', 'disable-dev-shm-usage', 'disable-extensions']
 });
-const driver = new Builder()
-    .usingServer(`http://selenium:4444`)
-    .forBrowser('chrome')
-    .withCapabilities(capabilities)
-    .build();
+const driver = new Builder().withCapabilities(capabilities).build();
 
 Given('The user navigates to the application', async function () {
     await driver.get(HOST);
