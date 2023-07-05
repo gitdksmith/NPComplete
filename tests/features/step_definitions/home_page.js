ac57ba05639108a76e9dff2a8846504b6b424623
gitdksmith <dksmith@protonmail.com>
2023-07-05 16:49:08 -0500

    #2

--- tests/features/step_definitions/home_page.js
+++ tests/features/step_definitions/home_page.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const { Given, When, Then, AfterAll } = require('@cucumber/cucumber');
 const { Builder, By, Capabilities, Key, WebDriver} = require('selenium-webdriver');
+const { Options } = require('selenium-webdriver/chrome');
 const expect = require('expect').expect;
 require("chromedriver");
 
@@ -12,7 +13,9 @@ capabilities.set('chromeOptions', {
     "w3c": false,
     "args": ['--headless', '--no-sandbox', '--disable-dev-shm-usage']
 });
-const driver = new Builder().withCapabilities(capabilities).build();
+const options = new Options().setChromeBinaryPath('usr/bin/google-chrome');
+// local /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
+const driver = new Builder().setChromeOptions(options).withCapabilities(capabilities).build();
 
 Given('The user navigates to the application', async function () {
     await driver.get(HOST);
